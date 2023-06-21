ca69398a364086c39aebd7100cad3e55a3a0ed26
SergioGuerra <guerrasergion@gmail.com>
2023-06-20 22:26:57 -0500

    Actualizacion de la clase estudiante, agregando el esquema de ejemplo

--- clase_estudiante.py
+++ clase_estudiante.py
@@ -8,3 +8,15 @@ class estudiante(BaseModel):
     direccion: str
     carrera: str
     semestre: int 
+
+    class Config:
+        schema_extra={
+            'ejemplo':{
+                'id':1000888999,
+                'nombre': 'Pepito',
+                'apellido': 'Perez',
+                'direccion': 'Cra 125c #00d 69',
+                'carrera': 'Ingenieria Informatica',
+                'semestre': 6
+            }
+        }
