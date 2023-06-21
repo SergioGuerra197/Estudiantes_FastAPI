4c61a177db6087d2b90d8ef90b208bb11c6a5040
SergioGuerra <guerrasergion@gmail.com>
2023-06-20 22:12:16 -0500

    Creacion de clase estudante e importacion de dependencias

---
+++ clase_estudiante.py
@@ -0,0 +1,3 @@
+from pydantic import BaseModel
+class estudiante(BaseModel):
+    id: int
---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+import clase_estudiante
+from pydantic import Field
+from typing import Optional, List
